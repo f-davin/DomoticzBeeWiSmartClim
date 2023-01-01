d80bab175ddf68a4c2439328b793c58f4339ba80
DavTechNet <8143811+DavTechNet@users.noreply.github.com>
2023-01-01 13:00:04 +0100

    Add specific class for sensor data and update log messages

--- plugin.py
+++ plugin.py
@@ -19,7 +19,7 @@
 #
 
 """
-<plugin key="BeewiSmartClim" name="BeeWi SmartClim" author="DavTechNet" version="0.5.0" externallink="https://github.com/DavTechNet/DomoticzBeeWiSmartClim">
+<plugin key="BeeWiSmartClim" name="BeeWi SmartClim" author="DavTechNet" version="0.5.0" externallink="https://github.com/DavTechNet/DomoticzBeeWiSmartClim">
     <description>
         <h2>BeeWi SmartClim</h2><br/>
         This plugin permits the following actions:
@@ -28,13 +28,11 @@
             <li>Read the humidity</li>
             <li>Read the battery level</li>
         </ul>
-        <h3>Configuration</h3>
-        One parameter exist, it's the MAC address of the device
     </description>
     <params>
-        <param field="Mode1" label="MAC address" required="true" default="XX:XX:XX:XX:XX:XX" width="200px" />
-        <param field="Mode2" label="Time between to measures in minuts" width="50px" required="true" default="15"/>
-        <param field="Mode6" label="Debug" width="100px">
+        <param field="Mode1" label="MAC address" required="true" default="XX:XX:XX:XX:XX:XX" width="300px" />
+        <param field="Mode2" label="Time between to measures in minutes" width="300px" required="true" default="15"/>
+        <param field="Mode6" label="Debug" width="300px">
             <options>
                 <option label="True" value="Debug"/>
                 <option label="False" value="Normal"  default="true" />
@@ -45,21 +43,93 @@
 """
 
 import subprocess
-import time
 from builtins import str
 from datetime import datetime
 from datetime import timedelta
 from enum import Enum, unique
-from subprocess import call, check_output, STDOUT
+from subprocess import check_output, STDOUT
 
-import Domoticz
+import DomoticzEx as Domoticz
 
 
 @unique
 class LogLevel ( Enum ):
+    """
+    Enumeration of the different log levels
+    """
     Notice = 0
     Error = 1
     Debug = 2
+
+
+@unique
+class HumidityStatus:
+    """
+    Enumeration of the different status of humidity
+    """
+    normal = 0
+    comfort = 1
+    dry = 2
+    wet = 3
+
+
+class SensorData:
+    def __init__ ( self, raw_data: bytearray ):
+        """
+        Initialize the values
+        :param raw_data: Data from the sensor
+        """
+        self.__temperature = 0.0
+        self.__humidity = 0
+        self.__battery = 0
+        self.parse_data ( raw_data )
+
+    def get_temperature ( self ) -> float:
+        """
+        Get the current temperature
+        :return: The current temperature
+        """
+        return self.__temperature
+
+    def get_humidity ( self ) -> int:
+        """
+        Get the current humidity
+        :return: The current humidity
+        """
+        return self.__humidity
+
+    def get_battery_level ( self ) -> int:
+        """
+        Get the current battery level
+        :return: The current battery level
+        """
+        return self.__battery
+
+    def parse_data ( self, raw_data: bytearray ):
+        """
+        Parse the dta from the sensor to extract the different values
+        :param raw_data: Data from sensor
+        :return: Nothing
+        """
+        if len ( raw_data ) != 10:
+            raise Exception ( 'Wrong size to decode data' )
+        # handle returns 10 byte hex block containing temperature, humidity and battery level
+        # the temperature consists of 3 bytes
+        # Positive value: byte 1 & 2 present the tenfold of the temperature
+        # Negative value: byte 2 - byte 3 present the tenfold of the temperature
+        # t0 = val [ 0 ]
+        # t1 = val [ 1 ]
+        # t2 = val [ 2 ]
+        # if t2 == 255:
+        #   temperature = (t1 - t2) / 10.0
+        # else:
+        #   temperature = ((t0 * 255) + t1) / 10.0
+        temperature = raw_data [ 2 ] + raw_data [ 1 ]
+        if temperature > 0x8000:
+            temperature = temperature - 0x10000
+        self.__temperature = temperature / 10.0
+        self.__humidity = raw_data [ 4 ]
+        self.__battery = raw_data [ 9 ]
 
 
 class BasePlugin:
@@ -85,20 +155,20 @@ class BasePlugin:
         return
 
     def onStart ( self ):
-        Domoticz.Log ( "onStart called" )
-        # Check if debug mmode is active
+        log_message ( LogLevel.Notice, "onStart called" )
+        # Check if debug mode is active
         self.device_address = Parameters [ "Mode1" ]
         if Parameters [ "Mode6" ] == "Debug":
             Domoticz.Debugging ( 1 )
-            # Domoticz.Log("Debugger started, use 'telnet 0.0.0.0 4444' to connect")
-            # import rpdb
-            # rpdb.set_trace()
-        if (len ( Devices ) == 0):
+            log_message ( LogLevel.Debug, "Debugger started, use 'telnet 0.0.0.0 4444' to connect" )
+            import rpdb
+            rpdb.set_trace ( )
+        if len ( Devices ) == 0:
             Domoticz.Device ( Name = "SmartClim", Unit = self.iUnit, TypeName = "Temp+Hum", Subtype = 1, Switchtype = 0,
                               Description = "Capteur SmartClim", Used = 1
                               ).Create ( )
-            Domoticz.Log ( "Device created." )
-        Domoticz.Log ( "Plugin has " + str ( len ( Devices ) ) + " devices associated with it." )
+            log_message ( LogLevel.Notice, "Device created." )
+        log_message ( LogLevel.Notice, "Plugin has " + str ( len ( Devices ) ) + " devices associated with it." )
 
         try:
             temperature, humidity, battery = self.getActualValues ( self.hci_device, self.device_address )
@@ -107,7 +177,7 @@ class BasePlugin:
                                             )
             self.nextMeasure = datetime.now ( ) + timedelta ( minutes = random.randrange ( 1, self.iDelayInMin, 1 ) )
         except (RuntimeError, NameError, TypeError):
-            Domoticz.Log ( "Error" )
+            log_message ( LogLevel.Error, "Error" )
 
         dump_config_to_log ( )
         Domoticz.Heartbeat ( 30 )
@@ -117,35 +187,37 @@ class BasePlugin:
             self.iDelayInMin = int ( Parameters [ "Mode2" ] )
         except ValueError:
             self.iDelayInMin = self.iDefaultDelayInMin
-        Domoticz.Log ( "Delay between measures " + str ( self.iDelayInMin ) + " minuts." )
-        Domoticz.Log ( "Leaving on start" )
+        log_message ( LogLevel.Notice, "Delay between measures " + str ( self.iDelayInMin ) + " minutes." )
+        log_message ( LogLevel.Notice, "Leaving on start" )
 
     def onStop ( self ):
-        Domoticz.Log ( "onStop called" )
+        log_message ( LogLevel.Notice, "onStop called" )
 
     def onConnect ( self, Connection, Status, Description ):
-        Domoticz.Log ( "onConnect called" )
+        log_message ( LogLevel.Notice, "onConnect called" )
 
     def onMessage ( self, Connection, Data ):
-        Domoticz.Log ( "onMessage called" )
+        log_message ( LogLevel.Notice, "onMessage called" )
 
     def onCommand ( self, Unit, Command, Level, Hue ):
-        Domoticz.Log (
-            "onCommand called for Unit " + str ( Unit ) + ": Parameter '" + str ( Command ) + "', Level: " + str (
-                Level
-                )
-            )
+        log_message ( LogLevel.Notice,
+                      "onCommand called for Unit " + str ( Unit ) + ": Parameter '" + str ( Command
+                                                                                            ) + "', Level: " + str (
+                          Level
+                          )
+                      )
 
     def onNotification ( self, Name, Subject, Text, Status, Priority, Sound, ImageFile ):
-        Domoticz.Log ( "Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str ( Priority
-                                                                                                         ) + "," + Sound + "," + ImageFile
-                       )
+        log_message ( LogLevel.Notice,
+                      "Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str ( Priority
+                                                                                                        ) + "," + Sound + "," + ImageFile
+                      )
 
     def onDisconnect ( self, Connection ):
-        Domoticz.Log ( "onDisconnect called" )
+        log_message ( LogLevel.Notice, "onDisconnect called" )
 
     def onHeartbeat ( self ):
-        Domoticz.Log ( "onHeartbeat called" )
+        log_message ( LogLevel.Notice, "onHeartbeat called" )
         if datetime.now ( ) > self.nextMeasure:
             # We immediatly program next connection for tomorrow, if there is a problem, we will reprogram it sooner
             try:
@@ -154,21 +226,23 @@ class BasePlugin:
             except:
 
                 try:
-                    Domoticz.Log ( "Restart the Bluetooth device" )
+                    log_message ( LogLevel.Notice, "Restart the Bluetooth device" )
                     self.cycleHci ( )
-                    Domoticz.Log ( "Bluetooth device restarted" )
+                    log_message ( LogLevel.Notice, "Bluetooth device restarted" )
                 except:
-                    Domoticz.Log ( "Error on restart" )
+                    log_message ( LogLevel.Error, "Error on restart" )
 
     def onGetSmartClimValues ( self ):
         temperature, humidity, battery = self.getActualValues ( self.hci_device, self.device_address )
-        humStat = self.getHumidityStatus ( temperature, humidity )
+        humStat = self.__get_humidity_status ( temperature, humidity )
         sValue = str ( temperature ) + ";" + str ( humidity ) + ";" + str ( humStat )
         self.updateDevice ( 0, sValue, battery )
 
     def updateDevice ( self, nValue, sValue, batteryLevel ):
         Devices [ self.iUnit ].Update ( nValue = 0, sValue = str ( sValue ), BatteryLevel = batteryLevel )
-        Domoticz.Log ( "Update " + str ( nValue ) + ":'" + str ( sValue ) + "' (" + Devices [ self.iUnit ].Name + ")" )
+        log_message ( LogLevel.Notice,
+                      "Update " + str ( nValue ) + ":'" + str ( sValue ) + "' (" + Devices [ self.iUnit ].Name + ")"
+                      )
 
     def getActualValues ( self, s_hci, s_mac ):
         # read the value characteristic (read handle is 0x003f)
@@ -197,9 +271,14 @@ class BasePlugin:
             battery = int ( octet_list [ 9 ], 16 )
         return (temperature, humidity, battery)
 
-    def getHumidityStatus ( self, temperature, humidity ):
-        # Temprature in °C
-        # Humudity in %
+    @staticmethod
+    def __get_humidity_status ( temperature: float, humidity: int ) -> HumidityStatus:
+        """
+        Convert the humidity percent in Domoticz value
+        :param temperature: Current temperature of the room in degree celsius
+        :param humidity: Current humidity of the room in percent
+        :return: The humidity status
+        """
         c1 = -8.78469475556
         c2 = 1.61139411
         c3 = 2.33854883889
@@ -209,30 +288,20 @@ class BasePlugin:
         c7 = 0.002211732
         c8 = 0.00072546
         c9 = -0.000003582
-        humStat = 0
+        ret = HumidityStatus.normal
 
-        HI = c1 + c2 * temperature + c3 * humidity + c4 * temperature * humidity + c5 * (
+        val = c1 + c2 * temperature + c3 * humidity + c4 * temperature * humidity + c5 * (
                 temperature * temperature) + c6 * (humidity * humidity)
-        HI = HI + c7 * (temperature * temperature) * humidity + c8 * temperature * (humidity * humidity) + c9 * (
+        val = val + c7 * (temperature * temperature) * humidity + c8 * temperature * (humidity * humidity) + c9 * (
                 temperature * temperature) * (humidity * humidity)
 
-        if HI >= 27 and HI < 32:
-            humStat = 1
-        if HI >= 32 and HI < 41:
-            humStat = 2
-        if HI >= 41:
-            humStat = 3
-
-        return humStat
-
-    def cycleHci ( ):
-        try:
-            call ( [ 'hciconfig', self.hci_device, 'down' ] )
-            time.sleep ( 0.1 )
-            call ( [ 'hciconfig', self.hci_device, 'up' ] )
-            time.sleep ( 0.1 )
-        except TypeError:
-            Domoticz.Log ( "Error on restart" )
+        if 27 <= val < 32:
+            ret = 1
+        elif 32 <= val < 41:
+            ret = 2
+        elif val >= 41:
+            ret = 3
+        return ret
 
 
 global _plugin
@@ -259,9 +328,9 @@ def onMessage ( Connection, Data ):
     _plugin.onMessage ( Connection, Data )
 
 
-def onCommand ( Unit, Command, Level, Hue ):
+def onCommand ( Unit, Command, Level, Color ):
     global _plugin
-    _plugin.onCommand ( Unit, Command, Level, Hue )
+    _plugin.onCommand ( Unit, Command, Level, Color )
 
 
 def onNotification ( Name, Subject, Text, Status, Priority, Sound, ImageFile ):
